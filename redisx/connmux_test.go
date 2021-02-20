0d51b586060ef729941d9a92a32c5979164f4ecc
zeusliu <lz503338451@163.com>
2021-02-21 07:39:18 +0800

    Update connmux_test.go

--- redisx/connmux_test.go
+++ redisx/connmux_test.go
@@ -19,9 +19,9 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/garyburd/redigo/internal/redistest"
-	"github.com/garyburd/redigo/redis"
-	"github.com/garyburd/redigo/redisx"
+	"github.com/ZeusLicard/redigo/internal/redistest"
+	"github.com/ZeusLicard/redigo/redis"
+	"github.com/ZeusLicard/redigo/redisx"
 )
 
 func TestConnMux(t *testing.T) {
